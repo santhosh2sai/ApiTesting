5de7a2e4bb3a0643080ee99c7ccc47ae542b0ae9
Sai Santhosh Samudrala <ssanthosh9492@gmail.com>
2018-09-05 12:08:00 -0700

    -2

--- src/test/java/com/api/webApiTest/GetMethodTest.java
+++ src/test/java/com/api/webApiTest/GetMethodTest.java
@@ -1,4 +1,4 @@
-package com.api.webApiTest.helper;
+package com.api.webApiTest;
 
 import org.apache.http.HttpStatus;
 import org.junit.Assert;
@@ -7,15 +7,13 @@ import org.junit.Test;
 import com.api.webApiTest.helper.RestApiHelper;
 import com.api.webApiTest.model.RestResponse;
 
-
-
-public class GetTestMethod {
+public class GetMethodTest {
 	
 	@Test
 	public void testGetPingAlive() {
 		String url = "http://localhost:8080/laptop-bag/webapi/api/ping/helloworld";
 		RestResponse response = RestApiHelper.performGetRequest(url);
 		Assert.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
-		Assert.assertEquals("hi helloworld", response.getResponseBody());
+		Assert.assertEquals("Hi! helloworld", response.getResponseBody());
 	}
 }
