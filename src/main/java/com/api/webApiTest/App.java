c98fd1b8c349e58017f6059b6e8303431478df49
Sai Santhosh Samudrala <ssanthosh9492@gmail.com>
2018-09-04 18:20:33 -0700

    first Commit

---
+++ src/main/java/com/api/webApiTest/App.java
@@ -0,0 +1,13 @@
+package com.api.webApiTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
